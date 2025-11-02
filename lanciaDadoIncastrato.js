9d90f45f4a696b1059ed92f5a7d62553981348d6
dpalexandru <alexpoian91@gmail.com>
2025-11-02 10:55:16 +0530

    snack2 - bonus

--- lanciaDadoIncastrato.js
+++ lanciaDadoIncastrato.js
@@ -18,6 +18,37 @@ const lanciaDado = () => {
   })
 }
 
-lanciaDado()
+const creaLanciaDado = () => {
+  let ultimonumero = 0
+
+  return function lanciaDado() {
+    return new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+
+
+        const precedente = ultimonumero;
+        const numeroCasuale = Math.floor(Math.random() * 6) + 1;
+        const message = precedente === numeroCasuale
+          ? `Waw due volte ${numeroCasuale} incredibile!`
+          : numeroCasuale;
+        ultimonumero = numeroCasuale;
+        const fallito = Math.floor(Math.random() * 6) + 1;
+        if (fallito === 1) {
+          return reject("Il tuo dado si è incastrato!")
+        }
+
+        return resolve(`${message}`)
+
+      }, 3000)
+    })
+  }
+
+
+}
+
+const lanciaConMemoria = creaLanciaDado();
+lanciaConMemoria()
   .then(ris => console.log(`Risultato: ${ris}`))
   .catch(err => console.log(err))
+
